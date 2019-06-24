67d3f3ada05585f99e898a973060348b6cad8e64
varun <varun060@gmail.com>
2019-06-25 00:11:21 +1000

    Updated transaction and account classes for code quality

--- accountmanager/AccountmanagerApplication.java
+++ accountmanager/AccountmanagerApplication.java
@@ -3,6 +3,12 @@ package com.bank.account.accountmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Bootstrap class to load all spring boot features,
+ * It triggers all components in spring boot application
+ * @author varun
+ *
+ */
 @SpringBootApplication
 public class AccountmanagerApplication {
 
